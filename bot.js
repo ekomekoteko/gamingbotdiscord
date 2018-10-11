837eca92d5bec81bd92d3f6dda4249431c465e2c
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 03:36:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const UserBlocked = new Set();
- 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
+});
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
