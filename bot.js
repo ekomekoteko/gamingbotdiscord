a4a735e8a62040b49999dc6c180db0faf5510143
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 13:28:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2466,6 +2466,56 @@ message.channel.sendFile(canvas.toBuffer());
 
 
 
+const moment = require('moment');//npm install moment
+client.on('message', message => {
+             var prefix = "g!"
+if (message.content.startsWith(prefix + "time")) {
+let user = message.mentions.users.first();
+var args = message.content.split(" ").slice(1);
+var men = message.mentions.users.first();
+var heg;
+if(men) {
+heg = men
+} else {
+heg = message.author
+}
+var mentionned = message.mentions.members.first();
+var h;
+if(mentionned) {
+h = mentionned
+} else {
+h = message.member
+}
+moment.locale('ar-TN'); //TN
+var id = new  Discord.RichEmbed()
+.setColor("RANDOM")
+.setAuthor(`${heg.username}#${heg.discriminator} `,heg.avatarURL)
+.setDescription([`**الوقت**${moment().format('HH:mm:ss A')}${moment().format('YYYY/M/D')}**اليوم** :${moment().format('dddd')}**التاريخ**`])
+message.channel.send(id)
+};
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
