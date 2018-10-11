57abc934de9f4bac0fc714f538b6b8cbf6be4752
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 14:30:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2608,7 +2608,28 @@ if (command == "embed") {
 /////////////////////////////////
 
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '个welcome个');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome To  Server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SERVER NAME **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+    
 
 
 
