aeb981f956281979a2579552face8ec0295a6912
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 05:11:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,37 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const devs = ['411137717884289024'];
+const db = require('quick.db');
+const premium = ['411137717884289024']
+const client = new Discord.Client();   
+const bot = new Discord.Client();   
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');   
+const fs = require("fs"); 
+const canvas = require("canvas");
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
+const { Client, Util } = require('discord.js');  
+const UserBlocked = new Set(); 
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+const dateFormat = require('dateformat'); 
+const pretty = require('pretty-ms') 
+const prefix = '!';
+var table = require('table').table
+var ti={}  
+,spee={}
+,attentions={};
 
 
 //-----------------------------------------------------------------------------------------------------------------------------
