9b6b878172dda801dcac612e73f27195596b494c
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 12:38:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2444,31 +2444,6 @@ client.on("message", message => {
   });
 
 /////////////////////////////
-client.on('message', message => {
-    if (message.content.startsWith("个welcome个")) {
-                                    var mentionned = message.mentions.users.first();
-                var mentionavatar;
-                  if(mentionned){
-                      var mentionavatar = mentionned;
-                  } else {
-                      var mentionavatar = message.author;
-                      
-                  }
-                  let bot;
-                  if(message.author.bot) {
-                      bot = 'Bot'
-                  } else {
-                      bot = 'User'
-                  } 
-     var EsTeKnAN = new Discord.RichEmbed()
-     .setColor('RANDOM')
-     .setThumbnail(`${mentionavatar.avatarURL}`)
-     .addField("***شكرا الانضمامك الينا***" ,mentionavatar.username )
-     .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
-     .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
-      message.channel.sendEmbed(EsTeKnAN);
-     }
-   });
 
 
 
