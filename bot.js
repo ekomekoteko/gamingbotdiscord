0aaee1449b6f38f3fa357083fb9e25897d7c2427
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 12:19:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2391,7 +2391,16 @@ client.on('message',async message => {
 //////////////////////////////////////
 
 
+client.on('guildCreate', guild => {
+    
 
+    var star = new Discord.RichEmbed()
+    .setTitle("GAMING BOT")
+    .setColor(000000)
+    .setDescription(`***دخلت عند دلخ***`)                      
+              .setFooter(`GAMING BOT `, 'https://cdn.discordapp.com/attachments/387286451034783744/388013469275914240/thesilent_1x.jpg')
+        guild.owner.send(star)
+  })
 
 
 
