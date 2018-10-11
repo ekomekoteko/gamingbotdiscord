3be322cc9c9f2d1c738c3489f59d96570eb9305b
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 04:26:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4231,10 +4231,39 @@ client.on('message',async message => {
   }
 });
 
-////////////////////////////LAW 7AD b3at rsala fe 5as el bot
+////////////////////////////bot all server
 
 
-
+client.on('message', message => {
+    if(message.content == '-Bot-All-Server') {
+             if(!message.author.id === '411137717884289024') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    gbots = g.members.filter(m=>m.bot).size;
+    groles = g.roles.map(r=> {return r.name});
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server bots',gbots)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    Server Name : **${gname}**
+    Server MemberCount : **${gmemb} **
+            
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
 
 
 
