63c0ca7e9c30d620c28386f3c82dbad58a2eefff
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 11:15:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2254,7 +2254,20 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
  
 });
 
-
+///////////////////////////////////////////////
+/////////////welcome new
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', '个welcome个')
+if (!channel) return;
+channel.send({embed : embed});
+});
+///////////////////
 
 
 
