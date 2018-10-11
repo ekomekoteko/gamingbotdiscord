5c7cfdb94cceef9046d07cbc5b73d65b04b9bf98
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-11 03:32:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const UserBlocked = new Set();
-const prefix = 'g!'
+const prefix = 'g!';
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
