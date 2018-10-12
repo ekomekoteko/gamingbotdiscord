fdd6bdc61a69def9d6fbb8db81751a61cf1f7bf4
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-12 07:50:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,12 +145,14 @@ client.on("message", message => {
 :mailbox_with_mail: g!support ~ سيرفر الدعم
 :mailbox_with_mail: g!contact ~ ارسال اقتراح او لمراسلة صاحب البوت
 :mailbox_with_mail: g!report ~  احد سبك واهانك
-:mailbox_with_mail: g!owner<massange ~ ارسال رسالة لي اونر
+:mailbox_with_mail: g!owner<messange ~ ارسال رسالة لي اونر
 :mailbox_with_mail: g!server ~  معلومات عن السرفر 
-:mailbox_with_mail: g!draw<massange ~ يحط لك الكلام في صوره
-:mailbox_with_mail: g!embed<massange> ~ يحط لك الكلام في مربع
+:mailbox_with_mail: g!draw<messange ~ يحط لك الكلام في صوره
+:mailbox_with_mail: g!embed<messange> ~ يحط لك الكلام في مربع
 :mailbox_with_mail: g!time ~ لمعرفة الوقت 
 :mailbox_with_mail: g!dt ~ لمعرفة الوقت والتاريخ
+:mailbox_with_mail: g!sa<message> ~يخلي الكلام رساله صوتيه
+:mailbox_with_mail: g!animal ~ صور حلوانات
 `)
    message.author.sendEmbed(embed)
     
@@ -3160,7 +3162,7 @@ exports.help = {
     category: 'INFO'
 }
 
-////////////////
+////////////////photo Animal
 var prefix = "g!";
 var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
     client.on('message', message => {
@@ -3171,7 +3173,7 @@ var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.d
 message.channel.sendEmbed(cat);
     }
 });
-//////////////////////////////////
+//////////////////////////////////Klam fe SOt 
 client.on('message', message => {
     if(message.content.startsWith('g!sa')) {
     let args = message.content.split(' ').slice(1);
@@ -3180,6 +3182,22 @@ client.on('message', message => {
     message.channel.send(ar,{tts:true});
 }
 });
+
+///////////////////////////////////////////
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var iiMo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTimestamp()
+        .setTitle('``I have received a new DM !``')
+        .setThumbnail(`${message.author.avatarURL}`)
+        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+        .setFooter(`From **${message.author.tag} (${message.author.id})**`)
+    client.channels.get("499206701904297984").send({embed:iiMo});
+    }
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
