3a3ebb29ad643ccc37e519039dad471ea21fb8b6
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-12 14:20:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3344,9 +3344,9 @@ if(message.content.startsWith(prefix + 'channel')) {//Alpha Codes
       .setTitle(":low_brightness:Channel Info:small_orange_diamond: ")//GAMING
       .setColor("#9932CC")//GAMING
       .setDescription(`:low_brightness:Info about <#${channel.id}>\n:low_brightness:Channel ID: ${channel.id}:pen_ballpoint: `)//Alpha Codes
-      .addField(":low_brightness:Created At :small_red_triangle_down: ", `${channel.createdAt}:clock: `)//GAMING
-      .addField(":low_brightness:Channel Type :flag_white:", `${channel.type}:low_brightness:`)//GAMING
-      .addField(":low_brightness:Extra Information :100: ", `:low_brightness:Channel is NSFW => ${channel.nsfw}:round_pushpin: \n:low_brightness:Channel Topic=> ${channel.topic} \n:low_brightness: Channel Parent => ${channel.parent} :orange_book: \:low_brightness: nChannel Position =>   ${channel.position} :black_heart: `)
+      .addField(":low_brightness:Created At :small_red_triangle_down: ",  `:low_brightness: ${channel.createdAt}:clock: `)//GAMING
+      .addField(":low_brightness:Channel Type :flag_white:", `:low_brightness: ${channel.type}:low_brightness:`)//GAMING
+      .addField(":low_brightness:Extra Information :100: ", `:low_brightness:Channel is NSFW => ${channel.nsfw}:round_pushpin: \n:low_brightness:Channel Topic=> ${channel.topic} \n:low_brightness: Channel Parent => ${channel.parent} :orange_book: \n:low_brightness: Channel Position =>   ${channel.position} :black_heart: `)
  
      message.channel.send({ embed: embed });//GAMING
   }//GAMING
