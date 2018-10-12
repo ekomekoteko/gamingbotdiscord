5a4379bd607d1a751a199669e7813825bc94e3f5
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-12 07:56:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3140,27 +3140,6 @@ reaction2.on("collect", r => {
 
 ////////////////Ping
 
-exports.run = async (client, message, args, color) => {
-
-    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
-    let diff = (Date.now() - start); 
-    let API = (client.ping).toFixed(2)
-        
-        let embed = new Discord.RichEmbed()
-        .setTitle(`🔔 Pong!`)
-        .setColor(0xff2f2f)
-        .addField("📶 Latency", `${diff}ms`, true)
-        .addField("💻 API", `${API}ms`, true)
-        message.edit(embed);
-      
-    });
-
-}
-
-exports.help = {
-    name: 'ping',
-    category: 'INFO'
-}
 
 ////////////////photo Animal
 var prefix = "g!";
