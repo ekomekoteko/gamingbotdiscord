d751630190bf80034c8c315492d2a958e187a40b
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-12 06:30:48 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3168,7 +3168,7 @@ const yourID = "411137717884289024"; //Instructions on how to get this: https://
 const setupCMD = "g!createrolemessage"
 let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
 const roles = ["Hacker", "Artist", "Public Relations", "Intern"]; Member
-const reactions = ["ًں’»", "ًں–Œ", "ًںکƒ", "ًں†•"]; Reaction Here | الريأكشن هنا
+const reactions = ["ًں’»", "ًں–Œ", "ًںکƒ", "ًں†•"];  
 const botToken = "التوكين"; /*You'll have to set this yourself; read more // AlphaCodes غير كلمة التوكين وحط التوكين حقك
                      here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
  
