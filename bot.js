2a59c30fb107607b26b6890ce0d1a563c411ea92
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-12 08:41:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3173,7 +3173,19 @@ client.on('message', function(message) {
     }
 });
 
-////////////////photo Animal
+////////////////RsaLa most5dem el BOT
+
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if (message.content.startsWith('g!bc-bot')){
+    if(!message.author.id === '') return;
+    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
+    client.users.forEach(m =>{
+    m.sendMessage(args)
+    })
+    }
+    });
 
 
 
