cf2588ff1d4aac436973d90f1a2f8992a7a206f1
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-13 06:35:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2116,18 +2116,38 @@ if(msg.content.startsWith ('g!server')) {
       .setColor('RANDOM')
       .setThumbnail(msg.guild.iconURL)
       .setTitle(`Showing Details Of  **${msg.guild.name}*`)
-      .addField(':globe_with_meridians:** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
-      .addField(':medal:** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
-      .addField(':red_circle:**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
-      .addField(':large_blue_circle:**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
-      .addField(':pencil:**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
-      .addField(':microphone:**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
-      .addField(':crown:**__ الأونـر__**',`**${msg.guild.owner}**`,true)
-      .addField(':id:**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
-      .addField(':date:**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+      .addField(':globe_with_meridians:**Sērvèr Typē**',`[** __${msg.guild.region}__ **]`,true)
+      .addField('trident:** __Rǎnks__**',`[** __${msg.guild.roles.size}__ **]`,true)
+      .addField(':red_circle:**__Númběr Thē  Mémběrs__**',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField(':large_blue_circle:**__Nūmbêr ǒf Mēmběrs  ónlīnè__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+      .addField(':pencil:**__ Rǒóms Typīng__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+      .addField(':speaking_head:**__Ròóms Vōicē__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+      .addField(':crown:**__Ownèr __**',`**${msg.guild.owner}**`,true)
+      .addField(':id:**__ID Sèrvèr__**',`**${msg.guild.id}**`,true)
+      .addField(':date:**__ Sērvér Frǒm This timě__**:tools:',msg.guild.createdAt.toLocaleString())
       msg.channel.send({embed:embed});
     }
   });
+///Server
+client.on('message', function(msg) {
+if(msg.content.startsWith ('g!Server')) {
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+      .addField(':globe_with_meridians:**Sērvèr Typē**',`[** __${msg.guild.region}__ **]`,true)
+      .addField('trident:** __Rǎnks__**',`[** __${msg.guild.roles.size}__ **]`,true)
+      .addField(':red_circle:**__Númběr Thē  Mémběrs__**',`[** __${msg.guild.memberCount}__ **]`,true)
+      .addField(':large_blue_circle:**__Nūmbêr ǒf Mēmběrs  ónlīnè__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+      .addField(':pencil:**__ Rǒóms Typīng__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+      .addField(':speaking_head:**__Ròóms Vōicē__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+      .addField(':crown:**__Ownèr __**',`**${msg.guild.owner}**`,true)
+      .addField(':id:**__ID Sèrvèr__**',`**${msg.guild.id}**`,true)
+      .addField(':date:**__ Sērvér Frǒm This timě__**:tools:',msg.guild.createdAt.toLocaleString())
+      msg.channel.send({embed:embed});
+    }
+  });
+
 
 ////////////////////
 
