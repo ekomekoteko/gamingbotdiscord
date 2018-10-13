8d96727b6bf655c32d4d5e8bed300c3897171664
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-13 08:32:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3623,7 +3623,21 @@ client.on('message', message => {
 
 
 //////////////////////////////
-
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith('g!ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms :signal_strength: ")
+.addField('**WebSocket:**',api + " ms :signal_strength: ")
+message.channel.send({embed:embed});
+}
+});
 
 
 
