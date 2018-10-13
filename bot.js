15768eb53196ad5296055aff19a0f51cf8f0cbf5
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-13 00:29:55 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3586,7 +3586,51 @@ client.on('message', message => {
 
 
 
-/////
+//////////////////////////////
+const config = require('./config.json');
+const prefix = config.prefix;
+const allowedUsers = config.allowedUsers;
+const role = config.roleToDisco;
+
+
+
+client.on("message", message => {
+
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    role.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      theRole.edit({color: random}).catch(e => {
+      });
+    });
+  }
+
+  if(message.content.startsWith(prefix + "startdisco")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nDiscoing...```");
+    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
+  } else {
+    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+} else
+
+if(message.content.startsWith(prefix + "stopdisco")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.send("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+}
+
+});
+
+client.on("ready", () => {
+    client.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
+    console.log("Disco role bot online! Created by i am toast.");
+});
+
 
 
 
