71f0fc3327f92ec82e6de319e298d108d7b94441
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-15 08:24:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4771,13 +4771,13 @@ client.on('message',message =>{
     }else{
         invs[inv.code] =+ inv.uses;
     }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+        invites.push(`:pushpin:invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\:pushpin: `;`);
    
   });
   var embed = new Discord.RichEmbed()
   .setColor("#000000")
   .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-  .setThumbnail("https://media.discordapp.net/attachments/477570106755383307/479229377037598720/22713057_151850495552450_709700562_o.jpg?width=201&height=201")
+  .setThumbnail("https://cdn.pg.sa/DDmkm6YzNO.jpg")
            message.channel.send({ embed: embed });
    
   });
