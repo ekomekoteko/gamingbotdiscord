350ea4081b3c2eb6bef804899d928eee8be76586
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-15 09:10:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5226,6 +5226,20 @@ client.on('message',message =>{
     }
   });
 //////////////////////////
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'mypoints')) {
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+	let userData = points[message.author.id];
+	let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+	.setColor('#000000')
+	.setDescription(`نقاطك: \`${userData.points}\``)
+	message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+});
 
 
 
