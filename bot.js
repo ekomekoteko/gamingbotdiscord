24f9af55eda95c34dac070f713f8e5f314b9f86f
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-15 09:32:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5226,7 +5226,68 @@ client.on('message',message =>{
     }
   });
 //////////////////////////
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
 
+    //!addrole @andrew Dog Person
+    if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("❌ Sorry pal, you can't do that.");
+    if (args[0] == "help") {
+        message.reply("Usage: !addrole <user> <role>");
+        return;
+    }
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!rMember) return message.reply("Couldn't find that user, yo.");
+    let role = args.join(" ").slice(22);
+    if (!role) return message.reply("Specify a role!");
+    let gRole = message.guild.roles.find(`name`, role);
+    if (!gRole) return message.reply("Couldn't find that role.");
+
+    if (rMember.roles.has(gRole.id)) return message.reply("❌ They already have that role.");
+    await (rMember.addRole(gRole.id));
+
+    try {
+        await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    } catch (e) {
+        console.log(e.stack);
+        message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    }
+}
+
+module.exports.help = {
+    name: "addrole"
+}
+
+// Remove Role
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Îmi pare rău, nu poți face asta.");
+    if (args[0] == "help") {
+        message.reply("Foloseste: !removerole <user> <role>");
+        return;
+    }
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!rMember) return message.reply("Nu am putut găsi acel utilizator");
+    let role = args.join(" ").slice(22);
+    if (!role) return message.reply("Specificați un rol");
+    let gRole = message.guild.roles.find(`name`, role);
+    if (!gRole) return message.reply("Nu am găsit acest rol.");
+
+    if (!rMember.roles.has(gRole.id)) return message.reply("Ei nu au acel rol.");
+    await (rMember.removeRole(gRole.id));
+
+    try {
+        await rMember.send(`Ai piedut gradul ${gRole.name}`)
+    } catch (e) {
+        message.channel.send(`RIP to <@${rMember.id}>, Ai fost ${gRole.name} de la ei. Am încercat să le dăm, dar DM-urile lor sunt blocate.`)
+    }
+}
+
+module.exports.help = {
+    name: "removerole"
+}
 
 
 client.login(process.env.BOT_TOKEN);
