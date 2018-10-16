78dea76f9e93be61a5e0f5bae66a5e7a14e2f85a
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-16 10:46:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5433,14 +5433,13 @@ hours = 12;
  
 });
 ///////CODE EL AFK 
-const Canvas = require("canvas"); //npm i canvas
-let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
+let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
- 
+
   if (message.author.bot) return;
   if(!message.channel.guild)return;
   if (!profile[message.author.id]) profile[message.author.id] = {
-    tite: 'Super User',
+    tite: '>title',
     rep: 0,
     reps: 'NOT YET',
     lastDaily:'Not Collected',
@@ -5448,15 +5447,15 @@ client.on("message", message => {
     points: 0,
     credits: 1,
   };
- 
- 
-fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+
+
+fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
 });
- 
+
 client.on('message', message => {
- 
+
     if(message.content.startsWith(prefix + 'rep')) {
       if(!message.channel.guild) return;
                     moment.locale('en');
@@ -5471,22 +5470,22 @@ client.on('message', message => {
          message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
         }
        }
-       fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+       fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
 });
- 
+
 client.on("message", (message) => {
   let men = message.mentions.users.first()
- 
+
   if (message.author.bot) return;
     if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;
+	if(!message.channel.guild) return;
 if (message.content.startsWith(prefix + 'credit')) {
   if(men) {
     if (!profile[men.id]) profile[men.id] = {
     lastDaily:'Not Collected',
-    credits: 195956
+    credits: 1,
   };
   }
   if(men) {
@@ -5495,17 +5494,17 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
   message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
 }
 }
- 
+
 if(message.content.startsWith(prefix + "daily")) {
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
+ if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
-    profile[message.author.id].credits += 200
-     message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
+    profile[message.author.id].credits += 300
+     message.channel.send(`**${message.author.username} you collect your \`300\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
   }
- 
+
  let cont = message.content.slice(prefix.length).split(" ");
 let args = cont.slice(1);
 let sender = message.author
@@ -5526,7 +5525,7 @@ if(message.content.startsWith(prefix + 'trans')) {
             var mentionned = message.mentions.users.first();
 if (!profile[sender.id]) profile[sender.id] = {}
 if (!profile[sender.id].credits) profile[sender.id].credits = 200;
-fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
       var mando = message.mentions.users.id;
@@ -5537,9 +5536,9 @@ if (err) console.error(err);
       let mariam = message.author.username
 message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
 }
- 
+
       });
- 
+
       client.on('message', message => {
           if(!profile[message.author.id]) profile[message.author.id] ={
               points: 0,
@@ -5552,31 +5551,31 @@ message.channel.send(`**:moneybag: | ${message.author.username}, has transferrer
               profile[message.author.id].level = Math.floor(profile[message.author.id].level+1);
               message.channel.send(`**${message.author.username}, You leveld up to __${profile[message.author.id].level}__**`)
           }
-          fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+          fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
       })
- 
+
     client.on('message', message => {
         let tit = message.content.split(" ").slice(1).join(" ");
         if(message.content.startsWith(prefix + "title")) {
-        if(!profile[message.author.id].tite) profile[message.author.id].tite = "Hey im using Super"
+        if(!profile[message.author.id].tite) profile[message.author.id].tite = "Hey im using DarkSide"
         if(!tit) {
-            message.channel.send("**Usage: <title <something>**");
+            message.channel.send("**:large_orange_diamond: .title <something>**");
         } else {
             profile[message.author.id].tite = tit
-            message.channel.send(`:ok:`)
+            message.channel.send(`:ok: Done Dude !?`)
         }
         }
-        fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+        fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
     })
- 
+
     client.on("message", message => {
   if (message.author.bot) return;
-    if(!message.channel.guild) return;
-if (message.content.startsWith(prefix + ".A")) {
+	if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + "profile")) {
                                let user = message.mentions.users.first();
          var men = message.mentions.users.first();
             var heg;
@@ -5600,101 +5599,101 @@ if (message.content.startsWith(prefix + ".A")) {
               getvalueof = message.author;
             }
    var mentionned = message.mentions.users.first();
- 
+
     var client;
       if(mentionned){
           var client = mentionned;
       } else {
           var client = message.author;
- 
+
       }
-if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: "HypeLC User", rep: 0, lastDaily: "NOT COLLECTED"};
+if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: ">title", rep: 0, lastDaily: "NOT COLLECTED"};
             let Image = Canvas.Image,
             canvas = new Canvas(300, 300),
             ctx = canvas.getContext('2d');
-            fs.readFile("Super.png", function (err, Background) { //امتداد الصورة
+            fs.readFile("./powerx.png", function (err, Background) { //امتداد الصورة
             if (err) return console.log(err);
             let BG = Canvas.Image;
             let ground = new Image;
             ground.src = Background;
             ctx.drawImage(ground, 0, 0, 300, 300); // حجم الصورة
- 
+
 })
- 
- 
- 
+
+
+
                 let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
                 jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
- 
+
                         //ur name
-                        ctx.font = 'bold 16px kathen'; // حجم الخط و نوعه
-                        ctx.fontSize = '40px'; // عرض الخط
+                    ctx.font = 'bold 13px Comic Sans MS'; // حجم الخط و نوعه
+                        ctx.fontSize = '30px'; // عرض الخط
                         ctx.fillStyle = "#000000"; // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
                         ctx.fillText(`${getvalueof.username}`, 153, 173) // احداثيات اسمك
- 
+
                         //ur name
-                        ctx.font = 'bold 16px kathen'; // حجم الخط و نوعه
-                        ctx.fontSize = '40px'; // عرض الخط
-                        ctx.fillStyle = "#f1f1f1"; // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
+                    ctx.font = 'bold 13px Comic Sans MS'; // حجم الخط و نوعه
+                        ctx.fontSize = '30px'; 
+                        ctx.fillStyle = "#f1f1f1"; 
+                        ctx.textAlign = "center"; 
                         ctx.fillText(`${getvalueof.username}`, 151, 171) // احداثيات اسمك
- 
+
                         //credit
-                        ctx.font = "bold 12px kathen" // نوع الخط وحجمه
+                        ctx.font = "bold 12px Arial" // نوع الخط وحجمه
                         ctx.fontSize = '10px'; // عرض الخط
                         ctx.fillStyle = "#f1f1f1" // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
                         ctx.fillText(`$${profile[getvalueof.id].credits}`, 81, 159) // احداثيات المصاري
- 
+
                         //poits
-                        ctx.font = "bold 12px kathen" // ن
+                        ctx.font = "bold 12px Arial" // ن
                         ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
                         ctx.fillStyle = "#f1f1f1" // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].points}`, 221, 159) // احداثيات النقاط
- 
+                        ctx.fillText(`${profile[getvalueof.id].points}`, 223, 159) // احداثيات النقاط
+
                         //Level
-                        ctx.font = "bold 27px kathen" // نوع الخط و حجمه
+                        ctx.font = "bold 20px Arial" // نوع الخط و حجمه
                         ctx.fontSize = '10px'; // عرض الخط
                         ctx.fillStyle = "#f1f1f1" // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].level}`, 221, 118) // احداثيات اللفل
- 
+                        ctx.fillText(`${profile[getvalueof.id].level}`, 223, 115) // احداثيات اللفل
+
                          //info
-                        ctx.font = "bold 12px kathen" // ن
+                        ctx.font = "bold 12px Arial" // ن
                         ctx.fontSize = '15px'; // عرض الخطوع الخط وحجمه
                         ctx.fillStyle = "#000000" // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
                         ctx.fillText(`${profile[getvalueof.id].tite}`, 150, 199) // احداثيات النقاط
- 
+
                         //info
-                        ctx.font = "bold 12px kathen" // ن
+                        ctx.font = "bold 12px Arial" // ن
                         ctx.fontSize = '15px'; // عرض الخطوع الخط وحجمه
                         ctx.fillStyle = "#f1f1f1" // لون الخط
                         ctx.textAlign = "center"; // محاذا ة النص
                         ctx.fillText(`${profile[getvalueof.id].tite}`, 150, 197) // احداثيات النقاط
- 
+
                         // REP
-                        ctx.font = "bold 26px  kathen";
-                        ctx.fontSize = "50px";
+                        ctx.font = "bold 20px  Arial";
+                        ctx.fontSize = "40px";
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
-                        ctx.fillText(`+${profile[getvalueof.id].rep}`, 80,117)
- 
+                        ctx.fillText(`+${profile[getvalueof.id].rep}`, 80,115)
+
                         let Avatar = Canvas.Image;
                         let ava = new Avatar;
- 
+
 ava.src = buf;
                         ctx.beginPath();
                         ctx.arc(75, 100, 780, 0, Math.PI*2, true);
                         ctx.closePath();
                         ctx.clip();
-                        ctx.drawImage(ava, 116, 82, 72, 72);
- 
+                          ctx.drawImage(ava, 116, 82, 73, 74);
+
 message.channel.startTyping()
 message.channel.sendFile(canvas.toBuffer())
 message.channel.stopTyping()
@@ -5702,7 +5701,6 @@ message.channel.stopTyping()
 })
 }
 });
-
 
 
 
