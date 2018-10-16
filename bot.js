c7a1068d41ff61b57ae11f9e2a137783ca370d4b
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-16 09:57:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5433,7 +5433,25 @@ hours = 12;
  
 });
 ///////CODE EL AFK 
+client.on('message', message => {
+	 var prefix = "g!";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+ 
+
+if (command == "za5") {
+    let say = new Discord.RichEmbed()
+        .setTitle('Text emboss :')
+   message.channel.send(`\n ${zalgo(args.join(' '))}`);
+  }
+
+});
 
 client.login(process.env.BOT_TOKEN);
 
