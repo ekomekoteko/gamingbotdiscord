f2662dbda8e5c2d6c502aee4b7bf9d3b8e692148
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-17 13:16:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5997,133 +5997,57 @@ const zead = [
   }
 });
 /////////////--------------------------//////
- client.on("message", message => {
+client.on("message", message => {
   if (message.author.bot) return;
-	if(!message.channel.guild) return;
-if (message.content.startsWith(prefix + "profile")) {
-                               let user = message.mentions.users.first();
-         var men = message.mentions.users.first();
-            var heg;
-            if(men) {
-                heg = men
-            } else {
-                heg = message.author
-            }
-          var mentionned = message.mentions.members.first();
-             var h;
-            if(mentionned) {
-                h = mentionned
-            } else {
-                h = message.member
-            }
-            var ment = message.mentions.users.first();
-            var getvalueof;
-            if(ment) {
-              getvalueof = ment;
-            } else {
-              getvalueof = message.author;
-            }
-   var mentionned = message.mentions.users.first();
-
-    var client;
-      if(mentionned){
-          var client = mentionned;
-      } else {
-          var client = message.author;
-
-      }
-if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET",credits: 1, level: 1,tite: ">title", rep: 0, lastDaily: "NOT COLLECTED"};
+    if(!message.channel.guild) return;
+ let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'pbc') {
+        if (!args[1]) {
+    message.channel.send(":white_check_mark: ***pbc <message>***");
+    return;
+    }
+        message.guild.members.forEach(member => {
+      if(!message.member.hasPermission('ADMINISTRATOR'))  return;
+      message.react("✅");
+            const w = ['./bc/1.png',
+                       './bc/2.png',
+                       './bc/3.png',
+                       './bc/4.png',
+                       './bc/5.png',
+                       './bc/6.png',
+                       './bc/7.png',];
             let Image = Canvas.Image,
-            canvas = new Canvas(300, 300),
+            canvas = new Canvas(400, 200),
             ctx = canvas.getContext('2d');
-            fs.readFile("./powerx.png", function (err, Background) { //امتداد الصورة
+            ctx.patternQuality = 'bilinear';
+        ctx.filter = 'bilinear';
+        ctx.antialias = 'subpixel';
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+        ctx.shadowOffsetY = 2;
+        ctx.shadowBlur = 2;
+            fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
             if (err) return console.log(err);
             let BG = Canvas.Image;
             let ground = new Image;
             ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 300, 300); // حجم الصورة
-
-})
-
-
-
-                let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
+            ctx.drawImage(ground, 0, 0, 400, 200);
+ 
+});
+ let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+               jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
-
-                        //ur name
-                    ctx.font = 'bold 13px Comic Sans MS'; // حجم الخط و نوعه
-                        ctx.fontSize = '30px'; // عرض الخط
-                        ctx.fillStyle = "#000000"; // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${getvalueof.username}`, 153, 173) // احداثيات اسمك
-
-                        //ur name
-                    ctx.font = 'bold 13px Comic Sans MS'; // حجم الخط و نوعه
-                        ctx.fontSize = '30px'; 
-                        ctx.fillStyle = "#f1f1f1"; 
-                        ctx.textAlign = "center"; 
-                        ctx.fillText(`${getvalueof.username}`, 151, 171) // احداثيات اسمك
-
-                        //credit
-                        ctx.font = "bold 12px Arial" // نوع الخط وحجمه
-                        ctx.fontSize = '10px'; // عرض الخط
-                        ctx.fillStyle = "#f1f1f1" // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`$${profile[getvalueof.id].credits}`, 81, 159) // احداثيات المصاري
-
-                        //poits
-                        ctx.font = "bold 12px Arial" // ن
-                        ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
-                        ctx.fillStyle = "#f1f1f1" // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].points}`, 223, 159) // احداثيات النقاط
-
-                        //Level
-                        ctx.font = "bold 20px Arial" // نوع الخط و حجمه
-                        ctx.fontSize = '10px'; // عرض الخط
-                        ctx.fillStyle = "#f1f1f1" // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].level}`, 223, 115) // احداثيات اللفل
-
-                         //info
-                        ctx.font = "bold 12px Arial" // ن
-                        ctx.fontSize = '15px'; // عرض الخطوع الخط وحجمه
-                        ctx.fillStyle = "#000000" // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].tite}`, 150, 199) // احداثيات النقاط
-
-                        //info
-                        ctx.font = "bold 12px Arial" // ن
-                        ctx.fontSize = '15px'; // عرض الخطوع الخط وحجمه
-                        ctx.fillStyle = "#f1f1f1" // لون الخط
-                        ctx.textAlign = "center"; // محاذا ة النص
-                        ctx.fillText(`${profile[getvalueof.id].tite}`, 150, 197) // احداثيات النقاط
-
-                        // REP
-                        ctx.font = "bold 20px  Arial";
-                        ctx.fontSize = "40px";
-                        ctx.fillStyle = "#f1f1f1";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`+${profile[getvalueof.id].rep}`, 80,115)
-
-                        let Avatar = Canvas.Image;
-                        let ava = new Avatar;
-
-ava.src = buf;
-                        ctx.beginPath();
-                        ctx.arc(75, 100, 780, 0, Math.PI*2, true);
-                        ctx.closePath();
-                        ctx.clip();
-                          ctx.drawImage(ava, 116, 82, 73, 74);
-
-message.channel.startTyping()
-message.channel.sendFile(canvas.toBuffer())
-message.channel.stopTyping()
-})
-})
+                       
+                        ctx.font = '17px Arial';
+                              ctx.fontSize = '13px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+              ctx.fillText(`${args} ` , 200, 100);
+member.sendFile(canvas.toBuffer());
+});
+});
+});
 }
 });
 
