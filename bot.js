8ca23f78da2100c46f5fd60be4fbde794aeaa172
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-17 06:51:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("",{type: 'watching'})
+ client.user.setActivity("By:Mahmoud-QuaStyle ",{type: 'watching'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -304,12 +304,11 @@ client.on("message", message => {
       .setColor("#000000")
       .setDescription(`
 	        اوامر الموسيقى 
- ╔[❖════════════❖]╗
- •
+╔[❖════════════❖]╗
+•
 تحت الصيانه
- •
-╚[❖════════════❖]╝`
-
+•
+╚[❖════════════❖]╝
  `)
    message.author.sendEmbed(embed)
     
