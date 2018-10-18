59c8a5fa1a3ad1ec52b0539d84868fec2ed8815f
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-18 07:08:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const client = new Discord.Client();
 
 client.on('ready', () => {
