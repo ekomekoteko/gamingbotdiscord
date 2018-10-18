72a29967c44f71dc33ed90fe2b93b49ac2f3993c
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-18 16:50:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6110,7 +6110,7 @@ client.on('message', async message => {
         return message.channel.send(embed4);
     }
 });
-////////////////
+////////////////set Count
 client.on('message',async message => {
     if(message.content.startsWith(prefix + "setCount")) {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
@@ -6129,7 +6129,30 @@ client.on('message',async message => {
     }
   });
 
+//////////////////////////////////////////////////Join in Voice 
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const YTDL = require('ytdl-core');
+const nodeopus = require('node-opus');
+const ffmpeg = require('ffmpeg');
+var servers = {};
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
 
+
+client.on('message', message =>{
+  if(message.content.startsWith('join')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("تم الأتصال بالروم الصوتي")
+}})
 
 
 
