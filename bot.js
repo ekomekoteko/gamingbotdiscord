041db048c2d840b615960a25b065abae778ebe02
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-18 06:55:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6119,6 +6119,23 @@ console.log('`Error`: ' + RebeL);
     }
 });
 
+////////////////Short
+client.on('message', message => { 
+	var prefix = "g!";
+ let args = message.content.split(' ').slice(1);
+    if(message.content.startsWith(prefix + 'short')) {
+    if(!message.channel.guild) return;  
+
+        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
+        googl.getKey();
+        googl.shorten(args.join(' ')).then(shorturl => {
+            message.channel.send(''+shorturl)
+        }).catch(e=>{
+            console.log(e.message);
+            message.channel.send('Error!');
+        });
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);
