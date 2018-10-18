c0dccb389255ae0e108171c8c29129f6c2e99c01
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-18 12:13:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var Canvas = require('canvas');// npm i canvas
+var jimp = require('jimp');// npm i jimp 
+const fs = require("fs");// npm i fs
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
