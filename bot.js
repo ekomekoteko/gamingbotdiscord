7c976f2059017f60b6ee80e1cdacfe48cded05bf
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-18 15:28:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6003,7 +6003,65 @@ console.log('`Error`: ' + RebeL);
 
 
 ////////////------------------------------------------------------//////Help test
- 
+ const serverStats = {
+    guildID: '479519956208320512',
+    ticketCategoryID: '411137717884289024'
+
+}
+
+
+//It's not a command, just send a DM for bot and... 
+
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type !== 'text') {
+        let active = await db.fetch(`support_${message.author.id}`);
+        let guild = client.guilds.get(serverStats.guildID);
+        let channel, found = true;
+        try {
+            if (active) client.channels.get(active.channelID)
+                .guild;
+        } catch (e) {
+            found = false;
+        }
+        if (!active || !found) {
+            active = {};
+            channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
+            channel.setParent(serverStats.ticketCategoryID)
+            channel.setTopic(`/complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
+
+            channel.overwritePermissions("465147579517370368", { //Role id (when someone join my server get this role with id <<, i dont know how to change it for @everyone. This will prevent users to see the channel, only admins will see!
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            });
+
+
+
+            let author = message.author;
+            const newChannel = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(author.tag, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+                .addField('User', author)
+                .addField('ID', author.id)
+            await channel.send(newChannel);
+            const newTicket = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(`Hello, ${author.username}`, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+            await author.send(newTicket);
+            active.channelID = channel.id;
+            active.targetID = author.id;
+        }
+        channel = client.channels.get(active.channelID);
+        const dm = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
+            .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
+        await message.author.send(dm);
+        if (message.content.startsWith(prefix + 'complete')) return;
+        const embe
 
 
 
