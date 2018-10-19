475f38922d4256e92c6b6cad21b8561006d04f51
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-19 10:50:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6572,6 +6572,46 @@ message.reply ("لتفعيل الرومات اكتب `g!Ch ac`");
 ///////////////////////
 
 
+//╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+//╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+//╭━╯┣━┳━━┳╮╭╮╭╮╭━━┳╮╭┳━╮╭━╯┣━━╮
+//┃╭╮┃╭┫╭╮┃╰╯╰╯┃┃╭━┫╰╯┃╭╮┫╭╮┃━━┫
+//┃╰╯┃┃┃╭╮┣╮╭╮╭╯┃╰━┫┃┃┃┃┃┃╰╯┣━━┃
+//╰━━┻╯╰╯╰╯╰╯╰╯╱╰━━┻┻┻┻╯╰┻━━┻━━╯
+
+
+
+client.on('message', message => {
+    var prefix = "-"
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+if(command == "draw") {
+     if(message.channel.type === "dm") return;
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '30px Impact';
+  let args = message.content.split(" ").slice(1);
+  
+Image.src = canvas.toBuffer();
+
+    console.log(Image);
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(args.join("  "),110, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+message.channel.sendFile(canvas.toBuffer());
+}
+});
 
 
 
