482f4869a631f6ce7425cf9241937bf46bd5551f
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-19 09:28:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6482,7 +6482,54 @@ function timeCon(time) {
 ////////////////////////////////////////---------------------------------------------------///////////////
 
 
+client.on('message', message => {
+if(message.content.startsWith('g!discrim') ) {
+     if(!message.channel.guild) return message.reply('** This command only for servers **')
+     
+          var args = message.content.split(" ").slice(1);
+    let sent = 0
+	let count = 1;
+	
+      if(args){
+client.users.filter(u => u.discriminator == args[0]).forEach(u => {
+    if(sent > 4){
+     return
+    }
+    sent = sent + 1
+      message.channel.send(`
+      ** ${count}➥ ${u.tag}**
+         
+      `)
+      count++;
+   
+      })
+      } 
+      
+}
 
+if(message.content ===('-discrim') ) {
+     if(!message.channel.guild) return message.reply('** This command only for servers **')
+  let sent = 0
+	let count = 1;
+          
+
+client.users.filter(u => u.discriminator == message.author.discriminator).forEach(u => {
+    if(sent > 4){
+        return
+    }
+    sent = sent + 1
+      message.channel.send(`
+      ** ${count}➥ ${u.tag}**
+         
+      `)
+      count++;
+   
+      })
+          
+      }
+
+ 
+});
 
 
 
