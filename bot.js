51bfc18603dc3b0e08d087bb6aa051a3f72fabc7
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-19 07:54:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6325,11 +6325,51 @@ client.on('message', message => {
 					timestamp: new Date(),
 					footer: {
 						icon_url: client.user.avatarURL,
-						text: "Powered by Google"
+						text: "GAMING BOT  by Google"
 					},
 				}});
 				
     }
+});
+///////////////////////////////////////////
+//╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+//╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+//╭━━┳╮╭┳━┳━━┳━╮╱╭━━┳╮╭┳━╮╭━╯┃
+//┃╭╮┃┃┃┃╭┫╭╮┃╭╮╮┃╭━┫╰╯┃╭╮┫╭╮┃
+//┃╰╯┃╰╯┃┃┃╭╮┃┃┃┃┃╰━┫┃┃┃┃┃┃╰╯┃
+//╰━╮┣━━┻╯╰╯╰┻╯╰╯╰━━┻┻┻┻╯╰┻━━╯
+//╱╱┃┃
+//╱╱╰╯
+
+client.on('message', message => {
+const yt = require('ytdl-core');
+  if (message.content.startsWith('-quran')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
+    }
+    voiceChannel.join()
+      .then(connnection => {
+        let stream = yt('https://www.youtube.com/watch?v=9-oGnGaI9Ps&t=8009s', {audioonly: true});
+        const dispatcher = connnection.playStream(stream);
+        dispatcher.on('end', () => {
+          voiceChannel.leave();
+        });
+      });
+  }
+  
+  if (message.content.startsWith('-stop')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
+    }
+voiceChannel.leave();
+  }
+
 });
 
 
