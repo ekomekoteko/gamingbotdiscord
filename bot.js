686ea4f3e42d01630d7329e4dda25fa3f4b41b7f
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-21 06:53:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7118,6 +7118,35 @@ hours = 12;
 ////////////////////////////////////Invite By:-------    bYTF3al anta el bf3aalo 
 
 ////////////By:Mahmoud-QuaStyle
+var math = require('mathjs') // npm i mathjs
+client.on("message", async msg => {
+
+    if (msg.channel.type !== "text") return undefined;
+
+    //if (msg.auhtor.bot) return undefined;
+
+    var args = msg.content.split(" ")
+
+    var prefix = "g!"
+
+  if (msg.content.toLowerCase().startsWith(prefix + "math")) {
+
+    if (!args[1]) return msg.channel.send("DiscordAPI Err : Missing args.")
+
+    if (args[1].length == 1) return msg.channel.send("JUST ONE NUMBER?????");
+
+    var count = parseInt(args[1]);
+
+    if (isNaN(count)) return msg.channel.send('No nigga');
+
+    try {
+      idk = await math.eval(args[1])
+    } catch (e) {
+      return msg.channel.send("ERRRRRRRRRRRRRRRRRRRRRRRR")
+    }
+    await msg.channel.send(idk)
+  }
+});
 
 
 
