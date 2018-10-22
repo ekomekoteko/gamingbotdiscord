72f96b22727f3270f326ac99f3c63c9eb32e0045
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-22 11:38:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7238,173 +7238,193 @@ hours = 12;
 
 ////////////By:Mahmoud-QuaStyle
 client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-  };
-if (message.content.startsWith(prefix + 'رياضيات')) {
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+    
+if(message.content.split(' ')[0] == prefix + 'id') {
+if(!message.channel.guild) return;
 
-const type = require('./gamesbombot/ryd.json');
-const item = type[Math.floor(Math.random() * type.length)];
-const filter = response => {
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**👑لديك 30 ثانية يلا ركز يا بطل👑**').then(msg => {
+let args = message.content.split(' ').slice(1).join(' ');
 
-            
-msg.channel.send(`${item.type}`).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **والله أنك ذكي وحش يا بطل**`);
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let points = {}
-            let userData = points[message.author.id];
-            let userdata = require('./points.json');
-            userData.points++;
-          })
-          .catch(collected => {
-            message.channel.send(`🕓😀أنتهى الوقت أعد العب مرة آخرى😀🕓`);
-          })
-        })
-    })
-}
-});
+       let defineduser = '';
+       if (!args[1]) { 
+           defineduser = message.author;
+       } else { // Run this if they did define someone...
+           let firstMentioned = message.mentions.users.first();
+           defineduser = firstMentioned;
+       }
 
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-  };
-if (message.content.startsWith(prefix + 'لغز')) {
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+       const w = ['./id5.png','./id6.png'];
+       var Canvas = require('canvas')
+var jimp = require('jimp')
 
-const type = require('./gamesbombot/quiz.json');
-const item = type[Math.floor(Math.random() * type.length)];
-const filter = response => {
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**👑لديك 30 ثانية لحل هذا الغز👑**').then(msg => {
+        const millis = new Date().getTime() - defineduser.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy');
+const stats2 = ['online', 'Low', 'Medium', 'Insane'];
+const days = millis / 1000 / 60 / 60 / 24;
+         dateFormat(now, 'dddd, mmmm dS, yyyy');
+             let time = `${dateFormat(defineduser.createdAt)}`
+             var heg;
+             if(men) {
+                 heg = men
+             } else {
+                 heg = message.author
+             }
+            var mentionned = message.mentions.members.first();
+              var h;
+             if(mentionned) {
+                 h = mentionned
+             } else {
+                 h = message.member
+             }
+       let Image = Canvas.Image,
+           canvas = new Canvas(300, 300),
+           ctx = canvas.getContext('2d');
+       ctx.patternQuality = 'bilinear';
+       ctx.filter = 'bilinear';
+       ctx.antialias = 'subpixel';
+ 
+       fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+           if (err) return console.log(err);
+           let BG = Canvas.Image;
+           let ground = new Image;
+           ground.src = Background;
+           ctx.drawImage(ground, 0, 0, 300, 300);
 
-            
-msg.channel.send(`${item.type}`).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **الجواب صحيح صح عليك**`);
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let points = {}
-            let userData = points[message.author.id];
-            let userdata = require('./points.json');
-            userData.points++;
-          })
-          .catch(collected => {
-            message.channel.send(`🕓😀أنتهى الوقت أعد العب مرة آخرى😀🕓`);
-          })
-        })
-    })
-}
-});
+})
+  var mentionned = message.mentions.users.first();
 
+   var client;
+     if(mentionned){
+         var client = mentionned;
+     } else {
+         var client = message.author;
+         
+     }
 
+var men = message.mentions.users.first();
+           var heg;
+           if(men) {
+               heg = men
+           } else {
+               heg = message.author
+           }
+               let url = defineduser.displayAvatarURL.endsWith(".webp") ? defineduser.displayAvatarURL.slice(20, 20) + ".png" : defineduser.displayAvatarURL;
+               jimp.read(url, (err, ava) => {
+                   if (err) return console.log(err);
+                   ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                       if (err) return console.log(err);
 
-              client.on('message', message => {
-                if (!points[message.author.id]) points[message.author.id] = {
-                  points: 0,
-                  };
-                if (message.content.startsWith(prefix + 'ركب')) {
-                  if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-                
-                const type = require('./gamesbombot/RKB.json');
-                const item = type[Math.floor(Math.random() * type.length)];
-                const filter = response => {
-                    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-                };
-                message.channel.send('**لديك 20 ثانيه لتركيب الكلمه**').then(msg => {
-                
+                       let Avatar = Canvas.Image;
+                       let ava = new Avatar;
+                       ava.src = buf;
+                       ctx.drawImage(ava, 112 , 40, 75, 75);
+                       
+                       
+                       
+                       
+                       var status;
+   if (defineduser.presence.status === 'online') {
+       status = 'ONLINE';
+ctx.fillStyle = `#2ce032`;
+ctx.beginPath();
+ctx.arc(179, 107, 10, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
+ 
+   } else if (defineduser.presence.status === 'dnd') {
+       status = 'DND';
+       ctx.fillStyle = `#ff0000`;
+ctx.beginPath();
+ctx.arc(179, 107, 8, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
+   } else if (defineduser.presence.status === 'idle') {
+       status = 'IDLE';
+       ctx.fillStyle = `#f4d32e`;
+ctx.beginPath();
+ctx.arc(179, 107, 10, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
+   } else if (defineduser.presence.status === 'offline') {
+       status = 'INVISIABLE';
+       ctx.fillStyle = `#898988`;
+ctx.beginPath();
+ctx.arc(179, 107, 10, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
+   }
+                       
+                       
+                                             var time2;
+     if(mentionned){
+         var time2 = `${dateFormat(message.mentions.users.first.joinedAt)}`;
+     } else {
+         var time2 = `${dateFormat(message.member.joinedAt)}`;
+         
+     }  
+                          
+   
+                       ctx.font = 'Bold 15px Arial ';
+                       ctx.fontSize = '15px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(status, 70 , 108 );
+                       
+                        ctx.font = 'Bold 13px Arial';
+                       ctx.fontSize = '13px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(`${message.author.presence.game === null ? "No Status" : message.author.presence.game.name}`, 150.00   , 180  );
+
                       
-                msg.channel.send(`${item.type}`).then(() => {
-                        message.channel.awaitMessages(filter, { maxMatches: 1, time: 20000, errors: ['time'] })
-                        .then((collected) => {
-                    message.channel.send(`${collected.first().author} ✅ **احسنت لقد تمكنت من تركيب الكلمه بسرعه**`);
-                    console.log(`[Typing] ${collected.first().author} typed the word.`);
-                            let won = collected.first().author;
-                            points[won.id].points++;
-                          })
-                          .catch(collected => {
-                            message.channel.send(`:x: **لم يتمكن احد من تركيب الكلمه في الوقت المناسب**`);
-                      console.log('[Typing] Error: No one type the word.');
-                          })
-                    })
-                  })
-                }
-                });
+                       ctx.font = 'Bold 20px Arial ';
+                       ctx.fontSize = '15px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(`${defineduser.username}`, 150.50 , 140);
+
+
+                       ctx.font = 'Bold 15px Arial';
+                       ctx.fontSize = '15px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(`#${defineduser.discriminator}`, 227  , 108);
+
+                       var time2;
+     if(mentionned){
+         var time2 = `${dateFormat(message.mentions.users.first.joinedAt)}`;
+     } else {
+         var time2 = `${dateFormat(message.member.joinedAt)}`;
+         
+     }
+
+                       ctx.font = 'Bold 13px Arial ';
+                       ctx.fontSize = '13px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(`${moment(defineduser.createdTimestamp).fromNow()}`, 179 , 226 );
+                       
+                       
+    
+          
+                       ctx.font = 'Bold 13px Arial ';
+                       ctx.fontSize = '13px';
+                       ctx.fillStyle = "#ffffff";
+                       ctx.textAlign = "center";
+                       ctx.fillText(`${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')}`, 179 , 253);
+                       
+message.channel.sendFile(canvas.toBuffer())
+
+
+       })
+   })
 
 
 
-            
-            client.on('message', message => {
-            if (!points[message.author.id]) points[message.author.id] = {
-                points: 0,
-              };
-            if (message.content.startsWith(prefix + 'شقلب')) {
-                if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-            
-            const type = require('./gamesbombot/SHAKLEB.json');
-            const item = type[Math.floor(Math.random() * type.length)];
-            const filter = response => {
-                return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-            };
-            message.channel.send('**لديك 15 ثانيه لشقلبة الكلمه**').then(msg => {
-            
-                        
-            msg.channel.send(`${item.type}`).then(() => {
-                    message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-                    .then((collected) => {
-                    message.channel.send(`${collected.first().author} ✅ **احسنت,لقد تمكنت من شقلبة الكلمة في الوقت المناسب**`);
-                    console.log(`[Typing] ${collected.first().author} typed the word.`);
-                        let won = collected.first().author;
-                        points[won.id].points++;
-                      })
-                      .catch(collected => {
-                        message.channel.send(`:x: **لم يتمكن احد من شقلبة الكلمه في الوقت المناسب**`);
-                        console.log('[Typing] Error: No one type the word.');
-                      })
-                    })
-                })
-            }
-            });
 
+}
 
-
-                client.on('message', message => {
-                  if (!points[message.author.id]) points[message.author.id] = {
-                    points: 0,
-                    };
-                  if (message.content.startsWith(prefix + 'كتابة')) {
-                    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-                  
-                  const type = require('./gamesbombot/type.json');
-                  const item = type[Math.floor(Math.random() * type.length)];
-                  const filter = response => {
-                      return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-                  };
-                  message.channel.send('** لديك 15 ثانيه لكتابه هذه الكلمه بسرعة**').then(msg => {
-                  
-                        
-                  msg.channel.send(`${item.type}`).then(() => {
-                          message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-                          .then((collected) => {
-                      message.channel.send(`${collected.first().author} ✅ **احسنت لقد تمكنت من كتابه هذه الكلمه بسرعه**`);
-                      console.log(`[Typing] ${collected.first().author} typed the word.`);
-                              let won = collected.first().author;
-                              points[won.id].points++;
-                            })
-                            .catch(collected => {
-                              message.channel.send(`:x: **لم يتمكن احد من كتابه هذه الكلمه في الوقت المناسب**`);
-                        console.log('[Typing] Error: No one type the word.');
-                            })
-                      })
-                    })
-                  }
-                  });
+})		
 
 
 
