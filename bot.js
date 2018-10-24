c8ec23b76fc56782e3845ca169ccf974f3ea6dbe
QuaStyle <43297529+QuaStyle@users.noreply.github.com>
2018-10-24 14:00:57 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1868,25 +1868,7 @@ client.on("guildMemberAdd", member => {
 
 }).catch(console.error)
 })
-/////////////////////Code law 7ad d5al server kolo y3mlo tr7eb gma3y 
 
-client.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-    newUsers[guild.id].set(member.id, member.user);
-  
-    if (newUsers[guild.id].size > 2) {
-      const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-      guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
-      newUsers[guild.id].clear();
-    }
-  });
-  
-  client.on("guildMemberRemove", (member) => {
-    const guild = member.guild;
-    if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-  });
-  
 
 
 
