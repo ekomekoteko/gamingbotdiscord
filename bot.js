3ac63909bd325e535a46024cb832a1ea139ee17a
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-14 06:13:28 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7680,7 +7680,6 @@ client.on("message", message => {
 
 
 ////////clan
-const fs = require('fs');
 const dat = JSON.parse(fs.readFileSync('./ClanSystem/invitedBy.json', 'UTF8'));
 client.on('ready',async () => {
   if(client.guilds.size !== 1) throw new Error("Client must be in 1 server.");
@@ -8125,61 +8124,6 @@ client.on('message',async message => {
 
 
 ////////////////////top
-client.on('ready',async () => {
-  console.log(`.Codes TOP.`);
-  hero.users.forEach(m => {
-    if(m.bot) return;
-    if(!tpoints[m.id]) tpoints[m.id] = {points: 0, id: m.id};
-    fs.writeFileSync("./Text.json", JSON.stringify(tpoints, null, 2));
-
-    if(!vpoints[m.id]) vpoints[m.id] = {points: 0, id: m.id};
-    fs.writeFileSync("./Voice.json", JSON.stringify(vpoints, null, 2));
-  });
-});
-
-client.on('message',async message => {
-  if(message.author.bot || message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let member = message.member;
-  let mention = message.mentions.users.first();
-  let guild = message.guild;
-  let author = message.author;
-
-  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
-  tpoints[author.id].points += rPoints;
-  fs.writeFileSync("./Text.json", JSON.stringify(tpoints, null, 2));
-  if(args[0] === `${client.config.prefix}top`) {
-    let _voicePointer = 1;
-    let _textPointer = 1;
-    let _voiceArray = Object.values(vpoints);
-    let _textArray = Object.values(tpoints);
-    let _topText = _textArray.slice(0, 5).map(r => `**\`.${_textPointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).sort((a, b) => a > b).join('\n');
-    let _voiceText = _voiceArray.slice(0, 5).map(r => `**\`.${_voicePointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).sort((a, b) => a > b).join('\n');
-
-    let topRoyale = new Discord.RichEmbed();
-    topRoyale.setTitle(' \ًں“‹Guild Score Leaderboards');
-    topRoyale.addField(`**TOP 5 TEXT ًں’¬**`, _topText, true);
-    topRoyale.addField(`**TOP 5 VOICE ًںژ™**`, _voiceText, true);
-    topRoyale.setFooter(message.author.username, message.author.avatarURL, message.author.tag);
-    topRoyale.setColor("GREEN");
-    message.channel.send(topRoyale).catch(e => {
-      if(e) return message.channel.send(`**. Error; \`${e.message}\`**`);
-    });
-  }
-});
-
-client.on('voiceStateUpdate', (u, member) => {
-  let author = member.user.id;
-  let guild = member.guild;
-  if(member.voiceChannel === null) return;
-  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
-  setInterval(() => {
-    if(!member.voiceChannel) return;
-    if(member.selfDeafen) return;
-    vpoints[author].points += rPoints;
-    fs.writeFileSync("./Voice.json", JSON.stringify(vpoints, null, 2));
-  }, 5000); // 5 Secs
-});
 
 
 
