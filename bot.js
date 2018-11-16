cf6525bcdb5bdb4c26807c01e0fbb0e0d90f8502
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-16 00:33:12 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7634,7 +7634,7 @@ client.on("message", message => {
 ////////tlb
 client.on('message', async message => {
   if(message.content.startsWith(prefix + "طلب")) {
-    await message.channel.send("**ماذا تريد**").then(e => {
+    await message.channel.send("**ماذا تريد:small_orange_diamond:**").then(e => {
     let filter = m => m.author.id === message.author.id
     let lan = '';
     let md = '';
@@ -7644,13 +7644,13 @@ client.on('message', async message => {
       lan = collected.first().content
       collected.first().delete()
 e.delete();
-     message.channel.send('**كم تدفع **').then(m => {
+     message.channel.send('**كم تدفع :small_blue_diamond:**').then(m => {
 let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
 .then(co => {
   md = co.first().content
         co.first().delete()
         m.delete();
-message.channel.send('**من الذي تشتري منه**').then(ms => {
+message.channel.send('**من الذي تشتري منه:shopping_cart:**').then(ms => {
 let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
 .then(col => {
   br = col.first().content
@@ -7658,16 +7658,16 @@ let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['
 
 ms.delete()
 
- message.channel.send('**انتظر....**').then(b => {
+ message.channel.send('** انتظر..**').then(b => {
         setTimeout(() => {
-  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت:white_check_mark:**`)
         },2000);
 var gg = message.guild.channels.find('name','طلب')
 if(!gg) return;
 if(gg) {
 gg.send({embed : new Discord.RichEmbed()
-.setDescription(`**ماذا تريد :scroll:    : \n ${lan}\nكم تدفع :moneybag: : \n ${md} \n طلبك :round_pushpin: : \n ${br}  \nمن الذي تشتري منه :top:  : <@${message.author.id}> **`)  
-          .setFooter(`ادارة السيرفر`)
+.setDescription(`**ماذا تريد:scroll: » \n ${lan}\nكم تدفع :moneybag: » \n ${md} \n طلبك :round_pushpin: » \n ${br}  \nمن الذي تشتري منه :top: » <@${message.author.id}> **`)  
+          .setFooter(`Mal,Shop Team♥`)
 .setTimestamp()
 });
 }        
