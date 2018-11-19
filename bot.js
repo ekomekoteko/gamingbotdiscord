0142afeeea45cda00791a76c58e51eec55c82ed0
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-19 10:30:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -235,12 +235,12 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on("message",message=> {                              //Wrong Code     3 Mistakes
+client.on('message',args=> {                              //Wrong Code     3 Mistakes
     let args = message.content.split(' ').slice(1);
     if(message.content.split(' ')[0] == '!poll') 
     var embed = new Discord.RichEmbed()
 
-    .setColor('#000000').setColor('#36393e')
+    .setColor('#000000').setColor('#36393e');
     addField('Poll',`${args}`,true)
 
     message.channel.send(embed)
