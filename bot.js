1132a3124b28ba7f157fcf886adef09aec0bafcd
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-21 02:48:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,12 +57,6 @@ client.on('guildMemberAdd', member => {
 });
 
 
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-  wait(1000);
 
   client.guilds.forEach(g => {
     g.fetchInvites().then(guildInvites => {
