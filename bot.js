9ad4eab7d79d0cc4e56cb1c0889e1a4a89022c2b
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-12-01 11:14:18 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4503,14 +4503,7 @@ message.channel.sendEmbed(cat);
     }
 });
 //////////////////////////////////Klam fe SOt 
-client.on('message', message => {
-    if(message.content.startsWith('g!55a')) {
-    let args = message.content.split(' ').slice(1);
-    let ar = args.join(' ');
 
-    message.channel.send(**#ar,{tts:true}**);
-}
-});
 
 ///////////////////////////////////////////
 client.on('message', function(message) {
